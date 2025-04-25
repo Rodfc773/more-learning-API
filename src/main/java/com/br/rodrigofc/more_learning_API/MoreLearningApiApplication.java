2fed50accd43ef6c67420be82a38dc2b5607157f
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2025-04-25 09:24:09 -0300

    Starting the Project

---
+++ src/main/java/com/br/rodrigofc/more_learning_API/MoreLearningApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.rodrigofc.more_learning_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreLearningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreLearningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/rodrigofc/more_learning_API/MoreLearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.rodrigofc.more_learning_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreLearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
