d190abe7c53684177a99e4f114db848e70be61e1
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2025-05-05 16:48:17 -0300

    feature: Adding a new route and new busines logic to fin by uid in the course domain

--- src/main/java/com/br/rodrigofc/more_learning_API/services/CourseService.java
+++ src/main/java/com/br/rodrigofc/more_learning_API/services/CourseService.java
@@ -2,10 +2,12 @@ package com.br.rodrigofc.more_learning_API.services;
 
 import com.br.rodrigofc.more_learning_API.models.CourseEntity;
 import com.br.rodrigofc.more_learning_API.repositories.CourseRepository;
+import com.br.rodrigofc.more_learning_API.exceptions.CourseNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class CourseService {
@@ -24,5 +26,9 @@ public class CourseService {
         } catch (Exception e) {
             throw new RuntimeException("Something went wrong");
         }
+    }
+
+    public CourseEntity getOne(UUID uuid){
+        return repository.findById(uuid).orElseThrow(CourseNotFound::new);
     }
 }
