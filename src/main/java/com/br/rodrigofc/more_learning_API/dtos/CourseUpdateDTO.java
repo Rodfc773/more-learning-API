7ab485554046f650d2ea34bc0c8854f5f668b056
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2025-05-12 20:53:51 -0300

    feat: Modifying the categorie's type in database to text[]

---
+++ src/main/java/com/br/rodrigofc/more_learning_API/dtos/CourseUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.br.rodrigofc.more_learning_API.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseUpdateDTO {
+
+    private String title;
+}
