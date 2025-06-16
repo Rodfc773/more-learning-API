bcc135649715f4b7f800115710097743b6631350
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2025-06-16 18:16:33 -0300

    test: Adding more unit tests for Course logic

--- src/test/java/com/br/rodrigofc/more_learning_API/unit/CourseServicerTest.java
+++ src/test/java/com/br/rodrigofc/more_learning_API/unit/CourseServicerTest.java
@@ -1,21 +1,24 @@
 package com.br.rodrigofc.more_learning_API.unit;
 
 import com.br.rodrigofc.more_learning_API.dtos.CourseResponseDTO;
+import com.br.rodrigofc.more_learning_API.dtos.CourseUpdateDTO;
 import com.br.rodrigofc.more_learning_API.models.CourseEntity;
 import com.br.rodrigofc.more_learning_API.repositories.CourseRepository;
 import com.br.rodrigofc.more_learning_API.services.CourseService;
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -30,6 +33,20 @@ public class CourseServicerTest {
 
     @Mock
     private CourseRepository courseRepository;
+
+    protected CourseEntity createSut() {
+
+        List<String> categories = new ArrayList<>();
+
+        categories.add("test");
+
+        return CourseEntity.builder().name("test").id(UUID.randomUUID()).category(categories).description("test").build();
+    }
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     @DisplayName("Should create a instance of Course")
@@ -49,6 +66,43 @@ public class CourseServicerTest {
         Assertions.assertNotNull(testObject);
         Assertions.assertEquals("Course Test", testObject.getCourseName());
         verify(courseRepository, times(1)).save(any(CourseEntity.class));
+    }
+
+    @Test
+    @DisplayName("Should update a instance if that instance exists")
+    public void shouldUpdateInstance(){
+
+        var entityTest = createSut();
+
+        CourseUpdateDTO courseUpdateDTO = CourseUpdateDTO.builder().name(Optional.of("teste 2")).description(Optional.of("teste teste")).build();
+
+        when(courseRepository.findById(entityTest.getId())).thenReturn(Optional.of(entityTest));
+
+        when(courseRepository.save(any(CourseEntity.class))).thenAnswer(invocation -> invocation.getArgument(0));
+        
+        var updateCourse = courseService.applyPatchUpdate(entityTest.getId(), courseUpdateDTO);
+
+        Assertions.assertNotNull(updateCourse);
+        Assertions.assertEquals(entityTest.getId(), updateCourse.getId());
+        Assertions.assertEquals("teste 2", updateCourse.getCourseName());
+        Assertions.assertEquals("teste tese", updateCourse.getDescription());
+
+        verify(courseRepository, times(1)).findById(any(UUID.class));
+        verify(courseRepository, times(1)).save(any(CourseEntity.class));
+    }
+    @Test
+    @DisplayName("Should delete a instance if that instance exists")
+    public void shouldDeleteCourseInstance(){
+
+        var entityTest = createSut();
+        var courseId = entityTest.getId();
+
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(entityTest));
+
+        courseService.deleteCourse(courseId);
+
+        verify(courseRepository, times(1)).deleteById(any(UUID.class));
+
     }
     @Test
     @DisplayName("Should not create a course if the name was not sent")
